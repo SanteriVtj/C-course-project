48af9309450e1fd5d342219c1c12b3fa9954e413
SanteriVtj <santeri.vaataja@gmail.com>
2020-08-18 15:01:33 +0300

    function name fixed

--- project.h
+++ project.h
@@ -7,7 +7,7 @@ struct student {
 };
 
 void printStudent(struct student *reg);
-struct student *addPparser(char *input, struct student *r);
+struct student *addParser(char *input, struct student *r);
 struct student *addStudent(struct student *reg, struct student *s);
 void refresP(char *input, struct student *reg);
 void del_register(struct student*r);
